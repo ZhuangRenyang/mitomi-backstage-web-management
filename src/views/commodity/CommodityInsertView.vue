0078fffc58829f01456ce04b907e5993f4494227
ZhuangRenyang <996491835@qq.com>
2022-12-05 22:25:49 +0800

    MiToMi商城后台管理

---
+++ src/views/commodity/CommodityInsertView.vue
@@ -0,0 +1,134 @@
+<template>
+  <div>
+    <el-card style="background-color: rgba(255,255,255,0.3)">
+      <h3>添加商品品牌</h3>
+      <br>
+      <el-breadcrumb separator-class="el-icon-arrow-right">
+        <el-breadcrumb-item :to="{ path: '/admin' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/admin/commodity/list">品牌管理</a></el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/admin/commodity/insert">添加商品</a></el-breadcrumb-item>
+      </el-breadcrumb>
+      <br>
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm">
+        <el-form-item label="商品名称" prop="name" label-width="120px" class="item">
+          <el-input style="width: 200px" type="text" v-model="ruleForm.name"></el-input>
+        </el-form-item>
+        <el-form-item label="商品分类" prop="pinyin" label-width="120px" class="item">
+          <el-input style="width: 200px" type="text" v-model="ruleForm.category"></el-input>
+        </el-form-item>
+        <el-form-item label="商品价格" prop="pinyin" label-width="120px" class="item">
+          <el-input style="width: 200px" type="text" v-model="ruleForm.price"></el-input>
+        </el-form-item>
+        <el-form-item label="商品LOGO" prop="logo" label-width="120px" class="item">
+          <el-upload
+              action="upload"
+              name="picFile"
+              :limit="1"
+              list-type="picture-card"
+              :on-preview="handlePictureCardPreview"
+              :on-success="handleSuccess"
+              :on-remove="handleRemove">
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt="">
+          </el-dialog>
+        </el-form-item>
+        <el-form-item label="类别id" prop="categoryId" label-width="120px" class="item">
+          <template>
+            <el-select style="width: 150px" placeholder="请选择">
+              <el-option></el-option>
+            </el-select>
+          </template>
+        </el-form-item>
+        <el-form-item label="品牌简介" prop="description" label-width="120px" class="item">
+          <el-input
+              style="width: 300px"
+              type="textarea"
+              placeholder="请输入内容"
+              v-model="ruleForm.description"
+              maxlength="1000"
+              show-word-limit
+
+          >
+          </el-input>
+        </el-form-item>
+        <el-form-item label="关键词列表" prop="keywords" label-width="120px" class="item">
+          <el-input style="width: 200px" type="text" v-model="ruleForm.keywords"></el-input>
+        </el-form-item>
+        <el-form-item label="排序查" prop="sort" label-width="120px" class="item">
+          <el-input style="width: 100px" type="text" v-model="ruleForm.sort"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button style="margin-left: 130px" type="primary" @click="submitForm('ruleForm')">提交</el-button>
+          <el-button @click="resetForm('ruleForm')">返回</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        category: '',
+        price:'',
+        logo: '',
+        categoryId: '',
+        description: '',
+        keywords: '',
+        sort: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入商品名称', trigger: 'blur'},
+          {min: 2, max: 15, message: '长度在 2 到 15 个字符', trigger: 'blur'}
+        ],
+        category: [
+          {required: true, message: '请输入商品分类', trigger: 'blur'},
+          {min: 2, max: 25, message: '长度在 2 到 25 个字符', trigger: 'blur'}
+        ],
+        // sort: [
+        //   {pattern: "/^[1-9]{1}[0-9]?$/", message: '必须是0-99之间的数值', trigger: 'blur'},
+        // ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          let url = "http://localhost:9080/commodity/insert"
+          this.axios.create({headers:{'Authorization':localStorage
+                  .getItem('jwt')}}).post(url, this.ruleForm).then((response) => {
+                console.log(response.data);
+                if (response.data.code == 20000) {
+                  this.$message({
+                    type: "success",
+                    message: "添加商品成功"
+                  })
+                } else {
+                  this.$message.error(response.data.message)
+                }
+              }
+          )
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
+<style>
+.item .el-form-item__label {
+  color: #acce8a;
+}
+</style>
---
+++ src/views/commodity/CommodityListView.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <el-card style="background-color: rgba(255,255,255,0.3)">
+      <br>
+      <h3>商品列表</h3>
+      <el-breadcrumb separator-class="/">
+        <el-breadcrumb-item :to="{ path: '/admin' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item><a href="/admin/commodity/list">商品列表</a></el-breadcrumb-item>
+        <el-breadcrumb-item><a href="">商品列表</a></el-breadcrumb-item>
+      </el-breadcrumb>
+      <br>
+      <el-table
+          :data="tableData"
+          border
+          style="width: 100%">
+        <el-table-column prop="id" label="编号" width="60"></el-table-column>
+        <el-table-column prop="name" label="商品名称" width="100"></el-table-column>
+        <el-table-column prop="category" label="商品分类" width="120"></el-table-column>
+        <el-table-column prop="price" label="商品价格" width="120"></el-table-column>
+        <!--        <el-table-column prop="logo" label="品牌logo"></el-table-column>-->
+        <!--        <el-table-column prop="description" label="品牌简介"></el-table-column>-->
+        <el-table-column prop="keywords" label="关键词"></el-table-column>
+        <el-table-column prop="positiveCommentCount" label="好评 / 评论数" width="150">
+          <template slot-scope="scope">
+            <span v-text="scope.row.positiveCommentCount"></span> /
+            <span v-text="scope.row.commentCount"></span>
+          </template>
+        </el-table-column>
+        <el-table-column prop="sales" label="销量" width="80"></el-table-column>
+        <el-table-column prop="productCount" label="总量" width="80"></el-table-column>
+        <el-table-column prop="sort" label="序号" width="80"></el-table-column>
+        <el-table-column prop="gmtCreate" label="创建时间" width="160"></el-table-column>
+        <el-table-column prop="gmtModified" label="修改时间" width="160"></el-table-column>
+        <el-table-column prop="enable" label="是否启用" width="80px">
+          <template slot-scope="scope">
+            <el-switch
+                v-model="scope.row.enable"
+                :active-value="1"
+                :inactive-value="0"
+                active-color="#13ce66"
+                inactive-color="#fdef34">
+            </el-switch>
+          </template>
+        </el-table-column>
+        <el-table-column prop="id" label="操作" width="180">
+          <template slot-scope="scope">
+            <el-button
+                size="mini"
+                @click=" brandEditOpen(scope.row.id)">修改
+            </el-button>
+            <el-button
+                size="mini"
+                type="danger"
+                @click="openDeleteConfirm(scope.row.id)">删除
+            </el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: []
+    }
+  },
+  methods: {
+    loadBrands: function () {
+      let url = "http://localhost:9080/commodity";
+      this.axios.create({headers:{'Authorization':localStorage
+              .getItem('jwt')}}).get(url).then((response) => {
+        console.log(response.data);
+        let jsonResult = response.data;
+        if (jsonResult.code == 20000) {
+          this.tableData = jsonResult.data;
+        } else {
+          this.$message.error(jsonResult.message)
+        }
+      })
+    },
+    brandEditOpen(id) {
+      this.$prompt('请输入类别名称', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        // inputPattern: "",
+        // inputErrorMessage: '格式不正确'
+      }).then(({value})=> {
+        this.brandEdit(id,value)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '取消输入'
+        });
+      });
+    },
+    brandEdit(id,value) {
+      let url = "http://localhost:9080/commodity/"+id+"/update/"+value
+      this.axios.create({headers:{'Authorization':localStorage
+              .getItem('jwt')}}).post(url).then((response) => {
+        if (response.data.code == 20000) {
+          this.$message({
+            type: "success",
+            message: "修改品牌名称成功"
+          })
+        } else {
+          this.$message.error(response.data.message)
+        }
+        this.loadBrands();
+      })
+      console.log(index,id);
+    },
+    openDeleteConfirm(id) {
+      this.$confirm('此操作将永久删除品牌数据, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.brandDelete(id)
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        });
+      });
+    },
+    brandDelete(id) {
+      let url = "http://localhost:9080/commodity/"+id+"/delete"
+      this.axios.create({headers:{'Authorization':localStorage
+              .getItem('jwt')}}).post(url).then((response) =>{
+        let jsonResult = response.data;
+        if (jsonResult.code == 20000){
+          this.$message({
+            type: "success",
+            message: "删除品牌成功"
+          })
+        }else {
+          this.$message.error(response.data.message)
+        }
+        this.loadBrands();
+      })
+    }
+  },
+  created() {//已创建，在mounted、显示页面之前执行
+    console.log("create")
+  },
+  mounted() {//已挂载，在created之后，在显示页面之前执行
+    console.log("mounted")
+    this.loadBrands();
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
